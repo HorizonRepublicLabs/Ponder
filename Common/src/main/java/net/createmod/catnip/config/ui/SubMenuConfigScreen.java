ed8c8941cc80fabda31fc61c0c149a11430e56e6
attackeight <71084901+Attack8@users.noreply.github.com>
2025-05-03 10:12:57 -0400

    Allow string type config editing (#11)
    Co-authored-by: IThundxr <me@ithundxr.dev>

--- Common/src/main/java/net/createmod/catnip/config/ui/SubMenuConfigScreen.java
+++ Common/src/main/java/net/createmod/catnip/config/ui/SubMenuConfigScreen.java
@@ -13,6 +13,11 @@ import java.util.function.Consumer;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import net.createmod.catnip.config.ui.entries.StringEntry;
+import net.createmod.ponder.enums.PonderGuiTextures;
+
+import net.minecraft.network.chat.Component;
+
 import org.lwjgl.glfw.GLFW;
 
 import com.electronwill.nightconfig.core.AbstractConfig;
@@ -281,6 +286,8 @@ public class SubMenuConfigScreen extends ConfigScreen {
 					entry = new EnumEntry(humanKey, (ForgeConfigSpec.ConfigValue<Enum<?>>) configValue, valueSpec);
 				} else if (value instanceof Number) {
 					entry = NumberEntry.create(value, humanKey, configValue, valueSpec);
+				} else if (value instanceof String) {
+					entry = new StringEntry(humanKey, (ForgeConfigSpec.ConfigValue<String>) configValue, valueSpec);
 				}
 
 				if (entry == null)
