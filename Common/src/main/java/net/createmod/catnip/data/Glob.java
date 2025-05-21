b620508427d1f84fb0c7db8e1f91edab4d0bb1a1
tobi-blackscale <71216944+tobi-blackscale@users.noreply.github.com>
2025-05-21 02:50:06 +0200

    Update Glob.java (#32)
    allow escaping backslashes as well.

--- Common/src/main/java/net/createmod/catnip/data/Glob.java
+++ Common/src/main/java/net/createmod/catnip/data/Glob.java
@@ -86,7 +86,7 @@ public class Glob {
 							if (i == globPattern.length()) {
 								throw new PatternSyntaxException("No character to escape", globPattern, i - 1);
 							}
-							if (next(globPattern, i) == ']' || next(globPattern, i)=='-') {
+							if (next(globPattern, i) == ']' || next(globPattern, i)=='-' || next(globPattern, i)=='\\') {
 								regex.append('\\');
 							}
 							regex.append(next(globPattern, i++));
