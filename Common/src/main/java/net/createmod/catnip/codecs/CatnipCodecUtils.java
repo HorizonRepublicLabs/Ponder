c211cd5a1049653e328a450057a1a2489fd0b55f
IThundxr <me@ithundxr.dev>
2025-11-02 22:04:58 -0500

    And extra helper methods for Nullable T/Tag in CatnipCodecUtils

--- Common/src/main/java/net/createmod/catnip/codecs/CatnipCodecUtils.java
+++ Common/src/main/java/net/createmod/catnip/codecs/CatnipCodecUtils.java
@@ -11,7 +11,10 @@ import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.Tag;
 import net.minecraft.resources.RegistryOps;
 
+import org.jetbrains.annotations.Nullable;
+
 public interface CatnipCodecUtils {
+	// Decode and return Optional<T>
 	static <T> Optional<T> decode(Codec<T> codec, Tag tag) {
 		return decode(codec, NbtOps.INSTANCE, tag);
 	}
@@ -24,6 +27,20 @@ public interface CatnipCodecUtils {
 		return codec.decode(ops, s).result().map(Pair::getFirst);
 	}
 
+	// Decode and return @Nullable T
+	static <T> @Nullable T decodeOrNull(Codec<T> codec, Tag tag) {
+		return decodeOrNull(codec, NbtOps.INSTANCE, tag);
+	}
+
+	static <T> @Nullable T decodeOrNull(Codec<T> codec, HolderLookup.Provider registries, Tag tag) {
+		return decodeOrNull(codec, RegistryOps.create(NbtOps.INSTANCE, registries), tag);
+	}
+
+	static <T, S> @Nullable T decodeOrNull(Codec<T> codec, DynamicOps<S> ops, S s) {
+		return codec.decode(ops, s).mapOrElse(Pair::getFirst, e -> null);
+	}
+
+	// Encode and return Optional<Tag>
 	static <T> Optional<Tag> encode(Codec<T> codec, T t) {
 		return encode(codec, NbtOps.INSTANCE, t);
 	}
@@ -34,5 +51,18 @@ public interface CatnipCodecUtils {
 
 	static <T, S> Optional<S> encode(Codec<T> codec, DynamicOps<S> ops, T t) {
 		return codec.encodeStart(ops, t).result();
+	}
+
+	// Encode and return @Nullable Tag
+	static <T> Tag encodeOrNull(Codec<T> codec, T t) {
+		return encodeOrNull(codec, NbtOps.INSTANCE, t);
+	}
+
+	static <T> Tag encodeOrNull(Codec<T> codec, HolderLookup.Provider registries, T t) {
+		return encodeOrNull(codec, RegistryOps.create(NbtOps.INSTANCE, registries), t);
+	}
+
+	static <T, S> S encodeOrNull(Codec<T> codec, DynamicOps<S> ops, T t) {
+		return codec.encodeStart(ops, t).mapOrElse(s -> s, e -> null);
 	}
 }
