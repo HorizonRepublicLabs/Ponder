30b99e6e0f3c9e4c668b08f2b6de289c9c364056
TropheusJ <jverrellijr@icloud.com>
2026-02-14 16:10:26 -0500

    more flywheel separation

---
+++ common/src/main/java/net/createmod/catnip/render/ShadersModHelper.java
@@ -0,0 +1,51 @@
+package net.createmod.catnip.render;
+
+import java.lang.constant.ClassDesc;
+import java.lang.constant.ConstantDescs;
+import java.lang.constant.DirectMethodHandleDesc;
+import java.lang.constant.MethodHandleDesc;
+import java.lang.constant.MethodTypeDesc;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.util.function.BooleanSupplier;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.createmod.catnip.platform.CatnipServices;
+import net.minecraft.util.Util;
+
+public final class ShadersModHelper {
+	private static final Logger logger = LogUtils.getLogger();
+
+	private static final DirectMethodHandleDesc handleDesc = MethodHandleDesc.ofMethod(
+		DirectMethodHandleDesc.Kind.STATIC,
+		ClassDesc.of("net.irisshaders.iris.api.v0.IrisApi"),
+		"isShaderPackInUse",
+		MethodTypeDesc.of(ConstantDescs.CD_boolean)
+	);
+
+	private static final BooleanSupplier isShaderPackInUse = Util.make(() -> {
+		if (!CatnipServices.PLATFORM.isModLoaded("iris"))
+			return () -> false;
+
+		try {
+			MethodHandle handle = handleDesc.resolveConstantDesc(MethodHandles.lookup());
+			return () -> {
+				try {
+					return (boolean) handle.invokeExact();
+				} catch (Throwable t) {
+					throw new RuntimeException("Exception checking for shaders", t);
+				}
+			};
+		} catch (ReflectiveOperationException e) {
+			logger.error("Failed to load Iris compat", e);
+			return () -> false;
+		}
+	});
+
+	public static boolean isShaderPackInUse() {
+		return isShaderPackInUse.getAsBoolean();
+	}
+}
