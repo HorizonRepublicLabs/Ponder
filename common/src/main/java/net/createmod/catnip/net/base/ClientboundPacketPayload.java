f6f6b11e0be2bfa0cd5dcb550b87f79253fbe1a6
IThundxr <me@ithundxr.dev>
2026-05-24 16:19:27 -0400

    Mark ClientboundPacketPayload#handle as @ClientOnly

--- common/src/main/java/net/createmod/catnip/net/base/ClientboundPacketPayload.java
+++ common/src/main/java/net/createmod/catnip/net/base/ClientboundPacketPayload.java
@@ -1,15 +1,15 @@
 package net.createmod.catnip.net.base;
 
+import net.createmod.catnip.annotations.ClientOnly;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.player.Player;
 
 public non-sealed interface ClientboundPacketPayload extends BasePacketPayload {
-	// TODO - Something
-
 	/**
 	 * Called on the main client thread.
 	 * Make sure that implementations are also annotated, or else servers may crash.
 	 */
+	@ClientOnly
 	void handle(LocalPlayer player);
 
 	default void handleInternal(Player player) {
